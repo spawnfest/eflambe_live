09a747ac46088d45be4baa2eef7c3072656c8cd4
Trevor Brown <admin@stratus3d.com>
2022-10-15 11:19:51 -0400

    Generate elixir application

---
+++ lib/eflambe_live.ex
@@ -0,0 +1,18 @@
+defmodule EflambeLive do
+  @moduledoc """
+  Documentation for `EflambeLive`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EflambeLive.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eflambe_live_test.exs
@@ -0,0 +1,8 @@
+defmodule EflambeLiveTest do
+  use ExUnit.Case
+  doctest EflambeLive
+
+  test "greets the world" do
+    assert EflambeLive.hello() == :world
+  end
+end
