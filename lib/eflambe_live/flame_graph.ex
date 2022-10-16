1063a5450a1a8ddc3a3f6d2f76f1d95ec222bc8f
Trevor Brown <admin@stratus3d.com>
2022-10-15 22:22:39 -0400

    Implement capture/3 and apply/2 functions

--- lib/eflambe_live/flame_graph.ex
+++ lib/eflambe_live/flame_graph.ex
@@ -1,4 +1,9 @@
 defmodule EflambeLive.FlameGraph do
+  @moduledoc """
+  Kino.JS flamegraph generator for Livebook. Accepts a flamegraph SVG as an
+  argument and displays it as an interactive SVG document in the livebook.
+  """
+
   use Kino.JS, assets_path: "lib/assets"
 
   def new(file) do
